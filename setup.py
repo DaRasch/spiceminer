6bb1e59b9ed2343e2fa1b56938c8b35420b097bd
Philipp Rasch <ph.r@hotmail.de>
2013-08-10 14:58:34 +0200

    Modified build system to use setuptools/distutils directly Added spiceminer/_spicewrapper.py. This is the ctypes wrapper around the c library

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@
 
 import os
 import sys
-import platform
-import subprocess
 
 try:
-    from setuptools import setup
+    from setuptools import setup, Extension
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, Extension
 
 
 # METADATA
@@ -21,51 +19,27 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+spice_path = os.path.join(os.getenv('CSPICEPATH', 'cspice'), 'include')
+if not os.path.isdir(spice_path):
+    print 'ERROR: spice not found'
+    sys.exit(1)
+
+cwrapper = Extension('spiceminer._libspice', ['cwrapper/ckgp.c'],
+    include_dirs=[spice_path])
+
 metadata = {
     'name': 'spiceminer',
     'version': version,
-    'description': "A sane Python wrapper for NASA's SPICE-framework.",
+    'description': "A sane python wrapper for NASA's SPICE-framework.",
     'long_description': readme,
     'author': 'Philipp Rasch',
     'author_email': 'ph.r@hotmail.de',
     'url': 'https://github.com/DaRasch/spiceminer',
     'download_url': 'https://github.com/DaRasch/spiceminer/archive/master.zip',
     'license': license,
-    'packages': ['spiceminer']
+    'packages': ['spiceminer'],
+    'ext_modules': [cwrapper],
+    'requires': ['numpy']
 }
 
-# COMMANDLINE HANDLING
-if 'build' in sys.argv[1:]:
-    sourcefiles = ['ckgp.c']
-    system = platform.system()
-    root = os.path.dirname(__file__)
-    # Check for cspice location
-    spice_path = os.getenv('CSPICEPATH', os.path.join(root, 'cspice'))
-    if not os.path.isdir(spice_path):
-        print 'ERROR: spice not found'
-    # Get other relevant paths
-    header_path = os.path.join(spice_path, 'include')
-    src_path = os.path.join(root, 'cwrapper')
-    # Compile so or dll
-    if system == 'Linux':
-        commandline = ['gcc', '-Wall', '-Werror', '-c', '-I' + header_path,
-        '-fPIC'] + [os.path.join(src_path, f) for f in sourcefiles]
-        try:
-            print subprocess.check_output(commandline) or 'gcc: objects built' #TODO check errors
-        except: pass
-        objectfiles = [f for f in os.listdir(src_path) if f.endswith('.o')]
-        commandline = ['gcc', '-Wall', '-Werror', '-shared', '-o',
-            os.path.join(root, 'spiceminer', '_libspicewrapper.so')] + objectfiles
-        try:
-            print subprocess.check_output(commandline) or 'gcc: lib built' #TODO check errors
-        except: pass
-    else: #TODO make platform independent
-        raise NotImplementedError('OS not supported')
-    # Clean up after build
-    for name in os.listdir(src_path):
-        if name.endswith('.o'):
-            os.remove(name)
-
-
-else:
-    setup(**metadata)
+setup(**metadata)
---
+++ spiceminer/_spicewrapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from ctypes import *
+
+cspice = CDLL('_libspice.so')
+
+
+def ckgp(spacecraft_id, instrument_id, et, tol, ref_frame):
+    cmat = c_int * 9
+    clkout = c_double()
+    found = c_int()
+    cspice.ckgp_c2(spacecraft_id, instrument_id, et, tol, ref_frame, cmat,
+        byref(clkout), byref(found))
+    if not found:
+        raise Exception():
