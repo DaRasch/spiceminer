c4737048d4a2ff6700c93e3d56f54105271c6e86
Philipp Rasch <ph.r@hotmail.de>
2013-08-17 09:20:44 +0200

    Fixed unitim.c and spkezr.c so that the c-extension actually compiles

--- cwrapper/unitim.c
+++ cwrapper/unitim.c
@@ -1,8 +1,7 @@
 #include "CustomSpice.h"
 
 char* unitim_custom(double* et, char* insys, char* outsys) {
-    SpiceDouble et_spice;
-    unitim_c(&et_spice, insys, outsys);
+    SpiceDouble et_spice = unitim_c(*et, insys, outsys);
     CHECK_EXCEPTION
     *et = (double) et_spice;
     FINALIZE
