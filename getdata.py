4f402e1d2ce911442197ac49899479eaa0f3c8d7
Philipp Rasch <ph.r@hotmail.de>
2013-09-02 01:26:25 +0200

    Added missions to getdata.py and fixed some errors in it

--- getdata.py
+++ getdata.py
@@ -13,16 +13,38 @@ from operator import itemgetter
 
 
 ### CONSTANTS ###
-NAMES = {'base': {'http://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/': 'de[0-9]*\.bsp',
-                  'http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/': 'pck[0-9]*\.tpc',
-                  'http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/': 'naif[0-9]*\.tls'},
-         'msl': {}}
+NAMES = {'base': {'generic_kernels/spk/planets/': ['de[0-9]*\.bsp'],
+                  'generic_kernels/pck/': ['pck[0-9]*\.tpc'],
+                  'generic_kernels/lsk/': ['naif[0-9]*\.tls']},
+         'msl': {'MSL/kernels/ck/': ['msl_ra_toolsref_v[0-9]*\.bc',
+                                    'msl_cruise_recon_rawrt_v[0-9]*\.bc',
+                                    'msl_cruise_recon_raweng_v[0-9]*\.bc',
+                                    'msl_edl_v[0-9]*\.bc',
+                                    'msl_surf_hga_tlm\.bc',
+                                    'msl_surf_ra_tlmenc\.bc',
+                                    'msl_surf_ra_tlmres\.bc',
+                                    'msl_surf_rsm_tlmenc\.bc',
+                                    'msl_surf_rsm_tlmres\.bc',
+                                    'msl_surf_rover_tlm\.bc'],
+                 'MSL/kernels/fk/': ['msl\.tf'],
+                 'MSL/kernels/sclk/': ['msl_lmst_ops[0-9]*_v[0-9]*\.tsc',
+                                      'msl.tsc'],
+                 'MSL/kernels/spk/': ['msl_struct_v[0-9]*\.bsp',
+                                     'mar[0-9]*s\.bsp',
+                                     'msl_cruise_v[0-9]*\.bsp',
+                                     'msl_edl_v[0-9]*\.bsp',
+                                     'msl_ls_ops[0-9]*_iau2000_v[0-9]*\.bsp',
+                                     'msl_surf_rover_tlm\.bsp']},
+         'helios': {'HELIOS/kernels/spk/': ['[0-9]*R_helios1_[0-9_]*\.bsp',
+                                           '[0-9]*R_helios2_[0-9_]*\.bsp']},
+         'ulysses': {'ULYSSES/kernels/spk/': ['ulysses_[0-9_]*\.bsp']}}
+BASE_URL = 'http://naif.jpl.nasa.gov/pub/naif/'
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 ### Evaluate command line ###
 def supported_data(arg):
     if arg.lower() not in NAMES:
-        msg = 'DATA must be in {}, was {}.'.format(NAMES.keys(), arg)
+        msg = 'DATA must be in {}, got {}.'.format(NAMES.keys(), arg)
         raise argparse.ArgumentTypeError(msg)
     return arg
 def valid_path(arg):
@@ -31,23 +53,26 @@ def valid_path(arg):
         raise argparse.ArgumentTypeError(msg)
     return arg
 parser = argparse.ArgumentParser()
-parser.add_argument('data', nargs='*', type=supported_data, default=NAMES.keys(),
-    metavar='DATA', help='The data to download (supported: {}).'.format(NAMES.keys()))
+parser.add_argument('data', nargs='*', type=supported_data,
+    default=NAMES.keys(), metavar='DATA',
+    help='The data to download (supported: {}).'.format(NAMES.keys()))
 parser.add_argument('--dir', '-d', nargs='?', type=valid_path,
     default=BASE_DIR, dest='path', help='Base directory for installation.')
 cmdline = parser.parse_args()
-print cmdline
 
 ### Get data ###
-def find_newest(pattern, site):
+def find_newest(pattern, text):
     regex_time = '[0-9a-zA-Z -]*:[0-9]*'
     regex_fill = '[a-zA-Z<> =/"]*'
     dt_format = '%d-%b-%Y %H:%M'
-    choices = re.findall(pattern + regex_fill + regex_time, site)
+    choices = re.findall(pattern + regex_fill + regex_time, text)
     choices = [(re.search(pattern, item).group(),
         dt.datetime.strptime(re.search(regex_time, item).group(), dt_format))
         for item in choices]
-    return sorted(choices, key=itemgetter(1), reverse=True)[0][0]
+    try:
+        return sorted(choices, key=itemgetter(1), reverse=True)[0][0]
+    except IndexError:
+        print 'Could not match pattern: {}'.format(pattern)
 
 def find_old(pattern, dir_path):
     return [name for name in os.listdir(dir_path) if re.search(pattern, name)]
@@ -60,22 +85,28 @@ def update(base_dir, name):
     except OSError as e:
         print e
     # Replace old or add new files
-    for url, pattern in NAMES[name].iteritems():
-        # Get old and new file names
-        old_files = find_old(pattern, data_dir)
-        site = urllib.urlopen(url)
-        newest = find_newest(pattern, site.read())
+    for url, patterns in NAMES[name].iteritems():
+        # Get old file names
+        old_files = []
+        for pattern in patterns:
+            old_files += find_old(pattern, data_dir)
+        # Get url source text
+        site = urllib.urlopen(BASE_URL + url)
+        text = site.read()
         site.close()
-        # Ignore files if old == new
-        if newest in old_files:
-            old_files.remove(newest)
-        # Download new
-        else:
-            print '{} >> {}'.format(newest, data_dir)
-            with open(os.path.join(data_dir, newest), 'wb') as f:
-                download = urllib.urlopen(url + newest)
-                f.write(download.read())
-                download.close()
+        # Replace/add
+        for pattern in patterns:
+            newest = find_newest(pattern, text)
+            # Ignore files if old == new
+            if newest in old_files:
+                old_files.remove(newest)
+            # Download new
+            else:
+                print '{} >> {}'.format(newest, data_dir)
+                with open(os.path.join(data_dir, newest), 'wb') as f:
+                    download = urllib.urlopen(BASE_URL + url + newest)
+                    f.write(download.read())
+                    download.close()
         # Remove old
         for f in old_files:
             try:
