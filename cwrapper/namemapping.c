b66639d7f957f81370848f9c541cf797b5a12d89
Philipp Rasch <ph.r@hotmail.de>
2013-09-02 20:39:46 +0200

    Merged bodn2c.c and bodc2n.c

--- cwrapper/namemapping.c
+++ cwrapper/namemapping.c
@@ -1,5 +1,14 @@
 #include "CustomSpice.h"
 
+char* bodc2n_custom(int code, char* name, int* found) {
+    /* name = malloc(sizeof(char) * STR_LEN); */
+    SpiceBoolean found_spice;
+    bodc2n_c(code, STR_LEN, name, &found_spice);
+    CHECK_EXCEPTION
+    *found = (int)found_spice;
+    FINALIZE
+}
+
 char* bodn2c_custom(char* name, int* code, int* found) {
     SpiceInt code_spice;
     SpiceBoolean found_spice;
