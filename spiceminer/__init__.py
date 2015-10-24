7dcc12375c325d702dd7dd5463d90ed189c69ef2
Philipp Rasch <ph.r@hotmail.de>
2015-10-24 22:07:00 +0200

    Change get to Body in __init__.py

--- spiceminer/__init__.py
+++ spiceminer/__init__.py
@@ -3,10 +3,10 @@
 from . import kernel
 
 from .time_ import Time
-from .bodies import get
+from .bodies import Body
 from ._spicewrapper import SpiceError
 from .extra import angle, frange, dtrange
 
-__all__ = ['kernel', 'get', 'Time', 'SpiceError', 'frange', 'dtrange']
+__all__ = ['kernel', 'Body', 'Time', 'SpiceError', 'frange', 'dtrange']
 
 __version__ = '0.1.0'
