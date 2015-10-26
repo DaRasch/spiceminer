50071380a285dc5b9b439323501776e03d2fbd27
Philipp Rasch <ph.r@hotmail.de>
2015-10-26 13:23:20 +0100

    Change imports in __init__.py to prepare for the API changes.

--- spiceminer/__init__.py
+++ spiceminer/__init__.py
@@ -1,12 +1,10 @@
 #-*- coding:utf-8 -*-
 
-from . import kernel
-
 from .time_ import Time
-from .bodies import Body
+from .bodies import *
+from .kernel import *
 from ._spicewrapper import SpiceError
-from .extra import angle, frange, dtrange
+#from .extra import angle, frange, dtrange
 
-__all__ = ['kernel', 'Body', 'Time', 'SpiceError', 'frange', 'dtrange']
 
 __version__ = '0.1.0'
