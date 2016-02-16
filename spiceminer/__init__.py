d41f218fc7600d4d5d99bf1557100a00e09c749a
Philipp Rasch <ph.r@hotmail.de>
2016-02-16 14:47:44 +0100

    Improve angle function and add cartesian <-> sphere conversion. Also make angle and conversion functions available by default.

--- spiceminer/__init__.py
+++ spiceminer/__init__.py
@@ -4,7 +4,7 @@ from .time_ import Time
 from .bodies import *
 from .kernel import *
 from ._spicewrapper import SpiceError
-#from .extra import angle, frange, dtrange
+from .extra import angle, cartesian2shpere, sphere2cartesian #, frange, dtrange
 
 
 __version__ = '0.1.0'
